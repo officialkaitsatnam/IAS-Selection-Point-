ad769a24a9f0cd8f6e0a71215408e029cf59fcb4
Satnam Kait <20393465+officialkaitsatnam@users.noreply.github.com>
2026-07-09 22:49:31 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// IAS Selection Point API Configuration - v19.1 Logo Header Fix
+// IAS Selection Point API Configuration - v19.2 Safe Layout + Logo Fix
 const API_URL = "https://script.google.com/macros/s/AKfycbx1Y4NvVZRoJy_ftslIsT52k6rSSbnJqMGcrbUM91bmcGZKgsPR8C1MmtBnJj055KOw/exec";
